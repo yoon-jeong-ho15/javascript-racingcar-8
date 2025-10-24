e7efd4e28f02c3cedb582cd67abfdd49cedd44a4
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-10-24 13:37:24 +0900

    feat(race): Implement race logic
    - create player object
    - repeat `processRound()` every rounds

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Console } from "@woowacourse/mission-utils";
+import { parseNames, parseRound } from "./parse.js";
+import { startGame } from "./race.js";
 
 class App {
   async run() {
@@ -10,7 +12,12 @@ class App {
     const inputRound = await Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
     );
-    const roundCount = parseRound(inputRound);
+    const round = parseRound(inputRound);
+
+    Console.print("\n실행 결과\n");
+    const winner = startGame(namesArr, round);
+
+    Console.print(`\n최종 우승자 : ${winner.join(", ")}\n`);
   }
 }
 
