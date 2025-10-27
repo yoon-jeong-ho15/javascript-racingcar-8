72cac8615dd89cce504c3108ab58e8588a433bef
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-10-27 17:41:00 +0900

    refactor(test): use test objects to mock player objects

--- __tests__/race.test.js
+++ __tests__/race.test.js
@@ -1,6 +1,23 @@
 import { createPlayers, processRound, findWinner } from "../src/race";
 import { getLogSpy, mockRandoms } from "../src/mocks";
 
+const testPlayers1 = [
+  { name: "hello", distance: 1 },
+  { name: "world", distance: 1 },
+];
+
+const testPlayers2 = [
+  { name: "my", distance: 1 },
+  { name: "name", distance: 0 },
+  { name: "is", distance: 2 },
+];
+
+const testPlayers3 = [
+  { name: "yoon", distance: 3 },
+  { name: "jeong", distance: 1 },
+  { name: "ho", distance: 3 },
+];
+
 describe("createPlayers 테스트", () => {
   test.each([
     [
@@ -25,45 +42,31 @@ describe("createPlayers 테스트", () => {
 });
 
 describe("processRound 테스트", () => {
-  const case1 = {
-    players: [
-      { name: "hello", distance: 0 },
-      { name: "world", distance: 0 },
-    ],
-    randoms: [8, 7],
-    logs: ["hello : -", "world : -"],
-    description: "모두 이동",
-  };
-  const case2 = {
-    players: [
-      { name: "yoon", distance: 1 },
-      { name: "jeong", distance: 1 },
-      { name: "ho", distance: 1 },
-    ],
-    randoms: [1, 2, 3],
-    logs: ["yoon : -", "jeong : -", "ho : -"],
-    description: "모두 제자리",
-  };
-  const case3 = {
-    players: [
-      { name: "my", distance: 1 },
-      { name: "name", distance: 1 },
-      { name: "is", distance: 1 },
-    ],
-    randoms: [1, 2, 3],
-    logs: ["my : -", "name : -", "is : -"],
-    description: "모두 제자리",
-  };
-
-  test.each([case1, case2, case3])(
-    "$description",
-    ({ players, randoms, logs }) => {
-      const logSpy = getLogSpy();
-      mockRandoms(randoms);
-      processRound(players);
-      logs.forEach((log) => {
-        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
-      });
-    }
-  );
+  test.each([
+    {
+      players: testPlayers1,
+      randoms: [8, 7],
+      logs: ["hello : -", "world : -"],
+      description: "모두 이동",
+    },
+    {
+      players: testPlayers2,
+      randoms: [1, 2, 3],
+      logs: ["my : -", "name : ", "is : --"],
+      description: "일부 이동",
+    },
+    {
+      players: testPlayers3,
+      randoms: [1, 2, 3],
+      logs: ["yoon : ---", "jeong : -", "ho : ---"],
+      description: "모두 제자리",
+    },
+  ])("$description", ({ players, randoms, logs }) => {
+    const logSpy = getLogSpy();
+    mockRandoms(randoms);
+    processRound(players);
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
 });
