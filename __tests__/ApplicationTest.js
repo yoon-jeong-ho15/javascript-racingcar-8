65ed686413da7f239832cc2a11765fde3c84b5f2
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-10-27 16:55:08 +0900

    refactor(test): move mocks.js to `/src` from `/__test__`

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,5 @@
 import App from "../src/App.js";
-import { getLogSpy, mockQuestions, mockRandoms } from "./mocks.js";
+import { getLogSpy, mockQuestions, mockRandoms } from "../src/mocks.js";
 
 describe("자동차 경주", () => {
   test("기능 테스트", async () => {
